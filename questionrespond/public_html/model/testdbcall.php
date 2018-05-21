00e561f31bebe1411879adfcbed088ee098def57
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:54:57 -0400

    Adding Comments

--- questionrespond/public_html/model/testdbcall.php
+++ questionrespond/public_html/model/testdbcall.php
@@ -1,10 +1,5 @@
  <?php
-      
-           
-            
-            
-
-
+ 
 include('databaseClass.php');
 
 $databaseClass = new databaseClass();
@@ -12,10 +7,10 @@ $databaseClass = new databaseClass();
 $connect = $databaseClass->connect();
 
 header("Content-Type: application/json; charset=UTF-8");
-$obj = json_decode($_GET["x"], false);
+$obj = json_decode($_GET["x"], false);//creating GET varible
 
 
-
+//select all query and encode it to JSON.
 $result = $connect->query("SELECT * FROM ".$obj->table);
 $outp = array();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
