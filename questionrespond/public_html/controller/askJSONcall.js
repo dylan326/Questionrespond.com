c276cb0e79c139c50919c9d5f60efaf0f0f4c742
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:25:36 -0400

    Adding comments

--- questionrespond/public_html/controller/askJSONcall.js
+++ questionrespond/public_html/controller/askJSONcall.js
@@ -7,14 +7,16 @@ xmlhttp.onreadystatechange = function() {
         myObj = JSON.parse(this.responseText);
         for (x in myObj) { 
           
-          
+          //Grab the user image to put in every loop.  and make the  question linked.
             txt += "<div class='askquestions'><img src='view/images/user.jpg' style='height: 50; width: 50;' > <b>" + myObj[x].username +":</b> "
             + "<b><a href='totalqs.php?q_id=" + myObj[x].q_id + "&question=" + myObj[x].question + "&username="+myObj[x].username +"'" + ">" + myObj[x].question + "</a></b> Asked on " 
             + myObj[x].q_date + "</div><br />" ;
           
         }
+      //this puts the JSON into the ask.php top level file
         document.getElementById("putquestionask").innerHTML = txt;
     }
 };
+//grabs the PHP file that turns the database call into JSON
 xmlhttp.open("GET", "../public_html/model/testdbcall.php?x="  + dbParam, true);
 xmlhttp.send();
