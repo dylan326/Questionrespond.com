a8d6f0f78a3ca03e97b92e1eceaacbeafe946350
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:27:16 -0400

    Adding comments

--- questionrespond/public_html/controller/checker.php
+++ questionrespond/public_html/controller/checker.php
@@ -1,10 +1,9 @@
 <?php
-
+//this file is included in all top level pages.  It checks if a session is started.  If not it re-directs to index.  
+//this is a safety feature so only users with usernames  can use the site. 
 if($_SESSION['username'] == "")
 {
     header("refresh:0; url=index.html");
     
 }
-
-
 ?>
