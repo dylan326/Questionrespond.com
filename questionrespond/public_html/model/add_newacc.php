bf4c9082e38a48125e2e08590c9592b09a1ec698
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:39:35 -0400

    Adding comments

--- questionrespond/public_html/model/add_newacc.php
+++ questionrespond/public_html/model/add_newacc.php
@@ -1,16 +1,17 @@
 <?php
     
     include('databaseClass.php');
-
+    //create object
     $databaseClass = new databaseClass();
     
+    //prevent XXS attacks with wash function
     $usernameWashed = $databaseClass->wash($_POST['username']);
 
     $emailWashed = $databaseClass->wash($_POST['email']);
 
     $passwordWashed = $databaseClass->wash($_POST['password']);
         
-    //$connect = $dbopen->connect();
+    //function to add user
 
     $userAdded = $databaseClass->insertuser($usernameWashed, $passwordWashed, $emailWashed);
    
@@ -22,7 +23,7 @@
     }
     else 
     {
-        header("refresh:11 url=../index.html");
+        header("refresh:5 url=../index.html");
         echo "Error:  Username is taken, please pick another username.  Re-directing to create account page, please hold...";
             
     }
