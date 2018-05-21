543ee87e87f48a2a42fe44add689d905b152cf99
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:37:04 -0400

    Adding comments

--- questionrespond/public_html/controller/yourqsJSONcall.js
+++ questionrespond/public_html/controller/yourqsJSONcall.js
@@ -1,6 +1,6 @@
 window.onload = function()
 {
- var obj, dbParam, xmlhttp, myObj, x, txt = "", username = document.getElementById("username_grab");
+ var obj, dbParam, xmlhttp, myObj, x, txt = "", username = document.getElementById("username_grab");//making username a JS var
 obj = { "table":"questions"};
 dbParam = JSON.stringify(obj);
 xmlhttp = new XMLHttpRequest();
@@ -8,6 +8,8 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         myObj = JSON.parse(this.responseText);
         for (x in myObj) { 
+         //if username pulled from session equals database call username output the questions
+         //this section is only for the users questions. 
           if(username.innerHTML == myObj[x].username)
           {
             txt += "<div id='yourqspage'>" + "<b><a href='totalqs.php?q_id=" + myObj[x].q_id + "&question=" + myObj[x].question +"&username=" + username.innerHTML + "'" + ">" 
@@ -17,7 +19,7 @@ xmlhttp.onreadystatechange = function() {
         document.getElementById("yourqs").innerHTML = txt;
     }
 };
-//xmlhttp.open("GET", "model/yourqsdbcall.php?x="  + dbParam, true);
+//grab file that turns the database call to JSON
 xmlhttp.open("GET", "model/testdbcall.php?x="  + dbParam, true);
 xmlhttp.send();
 };
