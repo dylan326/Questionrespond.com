71b1dc7423c03bb714644d77594f1d6386c360a6
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:52:55 -0400

    Adding comments

--- questionrespond/public_html/model/subq.php
+++ questionrespond/public_html/model/subq.php
@@ -1,16 +1,17 @@
 <?php
    session_start();
     include('databaseClass.php');
-    
+    //create object
     $databaseClass = new databaseClass();
-    //$connect = $dbopen->connect();
     
+    //prevent XXS attacks
     $questionWash = $databaseClass->wash($_POST['question']);
     
-    
+    //grab session varibles so it's possible to submit question correctly.
     $uname = $_SESSION['username'];
     $qa_email =$_SESSION['email'];
     
+   //method to insert question is called
     $questionSubmitted = $databaseClass->subq($questionWash, $uname, $qa_email);
     
     if ($questionSubmitted == true)
@@ -22,5 +23,5 @@
         echo "Error contact administrator" ;
     }
     
-    //$connect->close();
+    
  ?>
