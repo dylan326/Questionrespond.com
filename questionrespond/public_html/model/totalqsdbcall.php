d677ee631d6e220e9be2a88b6c0a9bb8cb581715
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:57:04 -0400

    Adding comments

--- questionrespond/public_html/model/totalqsdbcall.php
+++ questionrespond/public_html/model/totalqsdbcall.php
@@ -1,16 +1,16 @@
 <?php
       
 include('databaseClass.php');
-
+//create object
 $databaseClass = new databaseClass();
-
+//call connect method so I can use for query
 $connect = $databaseClass->connect();
            
 header("Content-Type: application/json; charset=UTF-8");
 $obj = json_decode($_GET["x"], false);
 
-
-$result = $connect->query("SELECT answer, q_id, a_date, cuser FROM ".$obj->table."");//where username = $username");
+//make specific results set and encode them to JSON. Results show users their questions and answers
+$result = $connect->query("SELECT answer, q_id, a_date, cuser FROM ".$obj->table."");
 $outp = array();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
 
