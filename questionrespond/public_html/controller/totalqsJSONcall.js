37d8553e9c9fbec5a81ac0699895a0b8940d80f9
Dylan Oldham <30761594+dylan326@users.noreply.github.com>
2018-05-21 10:34:19 -0400

    Adding comments

--- questionrespond/public_html/controller/totalqsJSONcall.js
+++ questionrespond/public_html/controller/totalqsJSONcall.js
@@ -1,6 +1,6 @@
 window.onload = function()
 {
-    var obj, dbParam, xmlhttp, myObj, x, txt = "", q_id = document.getElementById("q_id_grab");
+    var obj, dbParam, xmlhttp, myObj, x, txt = "", q_id = document.getElementById("q_id_grab");//pulling question id from php file into JS var
 obj = { "table":"answers"};
 dbParam = JSON.stringify(obj);
 xmlhttp = new XMLHttpRequest();
@@ -9,7 +9,8 @@ xmlhttp.onreadystatechange = function() {
         myObj = JSON.parse(this.responseText);
        
         for (x in myObj) {
-
+             //if the JSON q_id equals the q_id we pulled from the aswer page output.  
+            //this allows useres to only see their questions and answers
             if(q_id.innerHTML == myObj[x].q_id)
             {
             txt += "<div id='totalqsstyle'><img src='view/images/user.jpg' style='height: 50; width: 50;' >"  
@@ -18,10 +19,12 @@ xmlhttp.onreadystatechange = function() {
             
             
         }
+        //put JSON in top level file.
         document.getElementById("otheruseranswers").innerHTML = txt;
     }
     
 };
+    //grab the file that turns database call to JSON
 xmlhttp.open("GET", "model/totalqsdbcall.php?x="  + dbParam, true);
 xmlhttp.send();
 };
